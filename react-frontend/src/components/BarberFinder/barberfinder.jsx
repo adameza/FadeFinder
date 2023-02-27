45a97d71ca97e2f7d28698f807a1d00d72b8feff
Lovro Dukic <ldukic@calpoly.edu>
2023-02-26 20:52:11 -0800

    Fixed markers on map

--- react-frontend/src/components/BarberFinder/barberfinder.jsx
+++ react-frontend/src/components/BarberFinder/barberfinder.jsx
@@ -1,54 +1,43 @@
 import * as React from 'react'
 import { useMemo } from 'react'
 import Map, { Marker } from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 import Pin from './pin'
 
-const BARBERS = './barbers.json'
+import CITIES from './barbers.json'
 
 const TOKEN =
   'pk.eyJ1IjoibGR1a2ljIiwiYSI6ImNsZWx2aXkwdDBmOTAzeW96d3V5YWIzd3kifQ.RwD-huz-ONdzVHqJcpYRzg'
 
 export default function BarberFinder() {
-    // const pins = useMemo(
-    //   () =>
-    //     BARBERS.map((barber, index) => (
-    //       <Marker
-    //         key={`marker-${index}`}
-    //         longitude={barber.longitude}
-    //         latitude={barber.latitude}
-    //         anchor="bottom"
-    //         color='#61dbfb'
-    //       >
-    //       </Marker>
-    //     )),
-    //   []
-    // )
-
-  const pins = [
-    <Marker longitude={-120.655889043} latitude={35.300998796} anchor="bottom" color='#61dbfb'>
-    </Marker>,
-  ]
+  const pins = useMemo(
+    () =>
+      CITIES.map((barber, index) => (
+        <Marker
+          key={`marker-${index}`}
+          longitude={barber.longitude}
+          latitude={barber.latitude}
+          anchor="bottom"
+        >
+          <Pin />
+        </Marker>
+      )),
+    []
+  )
 
   return (
     <div>
       <Map
         initialViewState={{
-          longitude: -100,
           latitude: 40,
+          longitude: -100,
           zoom: 3.5,
         }}
+        mapStyle="mapbox://styles/mapbox/streets-v12"
         mapboxAccessToken={TOKEN}
         style={{ width: '100vw', height: '100vh' }}
-        mapStyle="mapbox://styles/mapbox/streets-v12"
       >
-        {/* <Marker
-          longitude={-120.655889043}
-          latitude={35.300998796}
-          anchor="bottom"
-        >
-          <Pin />
-        </Marker> */}
         {pins}
       </Map>
     </div>
