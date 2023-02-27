79966290d4f7bb7d53c13f804d4047da0f826761
Lovro Dukic <ldukic@calpoly.edu>
2023-02-27 14:01:04 -0800

    Added popups, fixed markers

--- react-frontend/src/components/BarberFinder/barberfinder.jsx
+++ react-frontend/src/components/BarberFinder/barberfinder.jsx
@@ -1,24 +1,31 @@
-import * as React from 'react'
-import { useMemo } from 'react'
-import Map, { Marker } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import './barberfinder.css'
+import * as React from 'react'
+import { useMemo, useState } from 'react'
+import Map, { Marker, Popup } from 'react-map-gl'
 
 import Pin from './pin'
 
-import CITIES from './barbers.json'
+import BARBERS from './barbers.json'
 
 const TOKEN =
   'pk.eyJ1IjoibGR1a2ljIiwiYSI6ImNsZWx2aXkwdDBmOTAzeW96d3V5YWIzd3kifQ.RwD-huz-ONdzVHqJcpYRzg'
 
 export default function BarberFinder() {
+  const [popupInfo, setPopupInfo] = useState(null)
+
   const pins = useMemo(
     () =>
-      CITIES.map((barber, index) => (
+      BARBERS.map((barber, index) => (
         <Marker
           key={`marker-${index}`}
           longitude={barber.longitude}
           latitude={barber.latitude}
           anchor="bottom"
+          onClick={(e) => {
+            e.originalEvent.stopPropagation()
+            setPopupInfo(barber)
+          }}
         >
           <Pin />
         </Marker>
@@ -39,6 +46,24 @@ export default function BarberFinder() {
         style={{ width: '100vw', height: '100vh' }}
       >
         {pins}
+
+        {popupInfo && (
+          <Popup
+            anchor="top"
+            closeButton={false}
+            longitude={Number(popupInfo.longitude)}
+            latitude={Number(popupInfo.latitude)}
+            onClose={() => setPopupInfo(null)}
+            style={{}}
+          >
+            <div>
+              {popupInfo.name}, {popupInfo.availability}
+              {/* <button>
+                Schedule Now
+              </button> */}
+            </div>
+          </Popup>
+        )}
       </Map>
     </div>
   )
