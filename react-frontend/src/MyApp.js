6d86f47ddc0c9377d4c4bffa69ee9684e65af87f
Brenden Sprague <brenden@Brendens-MacBook-Pro-2.local>
2023-03-03 13:46:52 -0800

    Added startup logo and animations

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,4 +1,10 @@
 import './App.css'
+import React, { useState, useEffect } from 'react';
+import BarberPole from './components/Home/loading';
+import './components/Home/startupScreenText.css'; // import the CSS file for the startup animation styles
+
+
+import Loading from './components/Home/loading'
 import CustomerLogin from './components/Customer-Login/customer-login'
 import CustomerNew from './components/Customer-Login/customer-new'
 import BarberLogin from './components/Barber-Login/barber-login'
@@ -7,11 +13,23 @@ import ClientRegistration from './components/ClientReg/clientreg'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
+  const [isStartingUp, setIsStartingUp] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsStartingUp(false);
+    }, 3000); // hide the animation after 3 seconds
+  }, []);
   return (
     <div className="App">
+      {isStartingUp && <BarberPole />}
+      <div className="loading-screen">
+      <h1 className="loading-screen__logo">FadeFinder</h1>
+    </div>
       <header className="App-header">
+        
         <BrowserRouter>
           <Routes>
+            <Route path="/loading" element={<Loading />} />
             <Route path="/customerlogin" element={<CustomerLogin />} />
             <Route path="/customerlogin/new" element={<CustomerNew />} />
             <Route path="/barberlogin" element={<BarberLogin />} />
