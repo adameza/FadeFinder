2e605a28d6db1e5f4d5ed4d9c8d1efe55b1e63a5
adameza <adam@mezanet.com>
2023-03-08 01:01:07 -0800

    barber changes

--- react-frontend/src/components/Barber-Avail/Table.jsx
+++ react-frontend/src/components/Barber-Avail/Table.jsx
@@ -12,13 +12,34 @@ function TableHeader() {
   )
 }
 
+function convertTime(date) {
+  let time = new Date(date)
+  let hrs = time.getHours()
+  let min = time.getMinutes()
+  if (hrs > 12) var post = 'PM'
+  else var post = 'AM'
+  hrs = hrs % 12
+  if (min < 10) return `${hrs}:0${min} ${post}`
+  return `${hrs}:${min} ${post}`
+}
+
+const getDay = (appDate) => {
+  var date = new Date(appDate)
+  date.setDate(date.getDate() + 1)
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat']
+  return days[date.getDay()]
+}
+
 function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
+    const day = getDay(row.startTime)
+    const start = convertTime(row.startTime)
+    const end = convertTime(row.endTime)
     return (
       <tr key={index}>
-        <td>{row.day}</td>
-        <td>{row.startTime}</td>
-        <td>{row.endTime}</td>
+        <td>{day}</td>
+        <td>{start}</td>
+        <td>{end}</td>
         <td>
           <button onClick={() => props.removeCharacter(index)}>Delete</button>
         </td>
