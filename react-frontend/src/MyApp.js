684f9c36c953236dc3a71855da67489eb8a394b8
adameza <adam@mezanet.com>
2023-03-09 23:21:50 -0800

    loading screen fix

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import BarberPole from './components/Home/loading';
 import './components/Home/startupScreenText.css'; // import the CSS file for the startup animation styles
 
+
 import Loading from './components/Home/loading'
 import CustomerLogin from './components/Customer-Login/customer-login'
 import CustomerNew from './components/Customer-Login/customer-new'
@@ -14,20 +15,20 @@ import BarberFinder from './components/BarberFinder/barberfinder'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [isStartingUp, setIsStartingUp] = useState(true)
+  const [isStartingUp, setIsStartingUp] = useState(true);
   useEffect(() => {
     setTimeout(() => {
-      setIsStartingUp(false)
-    }, 3000) // hide the animation after 3 seconds
-  }, [])
+      setIsStartingUp(false);
+    }, 3000); // hide the animation after 3 seconds
+  }, []);
   return (
     <div className="App">
       {isStartingUp && <BarberPole />}
       <div className="loading-screen">
-        <h1 className="loading-screen__logo">FadeFinder</h1>
-      </div>
+      <h1 className="loading-screen__logo">FadeFinder</h1>
+    </div>
       <header className="App-header">
-        
+    {!isStartingUp &&
         <BrowserRouter>
           <Routes>
             <Route path="/loading" element={<Loading />} />
@@ -39,7 +40,7 @@ function App() {
             <Route path="/barberavail" element={<BarberAvailability />} />
             <Route path="/" element={<BarberFinder />} />
           </Routes>
-        </BrowserRouter>
+        </BrowserRouter>}
       </header>
     </div>
   )
